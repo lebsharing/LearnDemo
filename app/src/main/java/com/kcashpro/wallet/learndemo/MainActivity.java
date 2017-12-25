07b4ed83542b317ee20d7d0ef07d06a18c789a09
Li Enbo <lebsharing@sina.com>
2017-12-25 15:51:59 +0800

    chnage two ,commit by command

--- app/src/main/java/com/kcashpro/wallet/learndemo/MainActivity.java
+++ app/src/main/java/com/kcashpro/wallet/learndemo/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //this is text
+        //修改二
+        //
     }
 }
